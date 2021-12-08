aa5275fb8960f0623e546bfd77ade63521d0b6e5
guricerin <chanbo1e9@gmail.com>
2021-12-08 13:39:27 +0900

    rebuild site: Wed Dec  8 13:39:27 JST 2021

--- docs/js/menu.js
+++ docs/js/menu.js
@@ -1,25 +1,33 @@
 'use strict';
 
 (function iifeMenu(document, window, undefined) {
-	var menuBtn = document.querySelector('.menu__btn');
-	var	menu = document.querySelector('.menu__list');
+    var menuBtn = document.querySelector('.menu__btn');
+    var menu = document.querySelector('.menu__list');
 
-	function toggleMenu() {
-		menu.classList.toggle('menu__list--active');
-		menu.classList.toggle('menu__list--transition');
-		this.classList.toggle('menu__btn--active');
-		this.setAttribute(
-			'aria-expanded',
-			this.getAttribute('aria-expanded') === 'true' ? 'false' : 'true'
-		);
-	}
+    function toggleMenu() {
+        menu.classList.toggle('menu__list--active');
+        menu.classList.toggle('menu__list--transition');
+        this.classList.toggle('menu__btn--active');
+        this.setAttribute(
+            'aria-expanded',
+            this.getAttribute('aria-expanded') === 'true' ? 'false' : 'true'
+        );
+    }
 
-	function removeMenuTransition() {
-		this.classList.remove('menu__list--transition');
-	}
+    function removeMenuTransition() {
+        this.classList.remove('menu__list--transition');
+    }
 
-	if (menuBtn && menu) {
-		menuBtn.addEventListener('click', toggleMenu, false);
-		menu.addEventListener('transitionend', removeMenuTransition, false);
-	}
+    if (menuBtn && menu) {
+        menuBtn.addEventListener('click', toggleMenu, false);
+        menu.addEventListener('transitionend', removeMenuTransition, false);
+    }
 }(document, window));
+
+(function disableDisqusAds(document) {
+    for (let elm of document.querySelectorAll('dsq-*')) {
+        if (elm.hasAttribute('sandbox')) {
+            elm.style.display = 'none';
+        }
+    }
+}(document));
